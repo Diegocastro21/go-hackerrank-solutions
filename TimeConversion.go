d415ee629ce7067b13bdd6d0d47d53818bfec87e
Diegocastro21 <diegocamilocastrooliveros@gmail.com>
2023-01-26 15:21:21 -0500

    Push Time Conversion - solution Hackerrank

---
+++ TimeConversion.go
@@ -0,0 +1,15 @@
+import (
+	"fmt"
+	"time"
+)
+func timeConversion(s string) string {
+    // Write your code here
+    layout := "03:04:05PM"
+    t, err := time.Parse(layout, s)
+
+    if err != nil {
+        return fmt.Sprintln(err)
+    }
+    
+    return fmt.Sprintln(t.Format("15:04:05"))
+}
